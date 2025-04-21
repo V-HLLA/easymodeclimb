d530920a17e957110646e11c3f42f673fe1645f1
V <downfiresword@gmail.com>
2025-04-21 03:13:35 -0300

    Made header more mobile friendly.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,14 +4,14 @@ import { ModeToggle } from "./ThemeToggle";
 export default function Header() {
   return (
     <header className="w-full border-b border-gray-200 dark:border-purple-700 bg-white dark:bg-purple-900">
-      <div className="max-w-6xl mx-auto px-4 py-4 flex items-center justify-between">
+      <div className="max-w-6xl mx-auto px-4 py-4 flex items-center justify-between gap-2 max-sm:flex-wrap max-sm:justify-center">
         <Link
           href="/"
           className="text-2xl font-bold text-gray-900 dark:text-white"
         >
           EasymodeClimb
         </Link>
-        <nav className="space-x-4">
+        <nav className="flex ml-auto justify-center items-center gap-2">
           <Link
             href="/recomended-champions"
             className="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition"
